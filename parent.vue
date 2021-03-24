9fa19b8d1f61c8dd7ff66da350222d2f4a05847a
402490410 <402490410@qq.com>
2021-03-24 15:29:37 +0800

    haha

---
+++ parent.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    parent
+</div>
+</template>
+<script>
+import {mapGetters,mapActions} from 'vuex';
+export default {
+props: [],
+components: {},
+data() {
+return {};
+},
+computed: {
+...mapGetters({}),
+},
+methods: {
+...mapActions({}),
+},
+mounted() {},
+};
+</script>
+<style scoped>
+</style>
